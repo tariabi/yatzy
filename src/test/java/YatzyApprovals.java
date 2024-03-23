5d0556a8cc8b8db36390909e1e84c7d639d04ccc
tarek <tarekriabi@gmail.com>
2024-03-23 07:35:15 +0100

    added approval for smallStraight

--- src/test/java/YatzyApprovals.java
+++ src/test/java/YatzyApprovals.java
@@ -201,4 +201,21 @@ public class YatzyApprovals {
             diceValues5
         );
     }
+
+    @Test
+    void approvals_verify_smallStraight() {
+        var diceValues1 = new Integer[]{1,2,3,4,5,6};
+        var diceValues2 = new Integer[]{1,2,3,4,5,6};
+        var diceValues3 = new Integer[]{1,2,3,4,5,6};
+        var diceValues4 = new Integer[]{1,2,3,4,5,6};
+        var diceValues5 = new Integer[]{1,2,3,4,5,6};
+        CombinationApprovals.verifyAllCombinations(
+            Yatzy::smallStraight,
+            diceValues1,
+            diceValues2,
+            diceValues3,
+            diceValues4,
+            diceValues5
+        );
+    }
 }
