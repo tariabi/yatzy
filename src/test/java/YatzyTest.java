fa1f4212a7ca78de15e535d41f00a49519dd0850
tarek <tarekriabi@gmail.com>
2024-03-23 04:44:43 +0100

    removed temporary variables for consistent assertions

--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -5,16 +5,12 @@ public class YatzyTest {
 
     @Test
     public void chance_scores_sum_of_all_dice() {
-        int expected = 15;
-        int actual = Yatzy.chance(2,3,4,5,1);
-        assertEquals(expected, actual);
+        assertEquals(15, Yatzy.chance(2,3,4,5,1));
         assertEquals(16, Yatzy.chance(3,3,4,5,1));
     }
 
     @Test public void yatzy_scores_50() {
-        int expected = 50;
-        int actual = Yatzy.yatzy(4,4,4,4,4);
-        assertEquals(expected, actual);
+        assertEquals(50, Yatzy.yatzy(4,4,4,4,4));
         assertEquals(50, Yatzy.yatzy(6,6,6,6,6));
         assertEquals(0, Yatzy.yatzy(6,6,6,6,3));
     }
