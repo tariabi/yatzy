6c6697908f76c9411799e7c7bd30573ebb9e209e
tarek <tarekriabi@gmail.com>
2024-03-23 14:49:01 +0100

    using calculator for full house

---
+++ src/main/java/FullHouseScoreCalculator.java
@@ -0,0 +1,34 @@
+public class FullHouseScoreCalculator implements IYatzyScoreCalculator {
+
+    private final int[] dices;
+
+    public FullHouseScoreCalculator(int[] dices) {
+        this.dices = dices;
+    }
+
+    @Override
+    public int computeScore() {
+        var tallies = new ScoreUtils().buildIntArrayFromParametersList(dices);
+        boolean _2 = false;
+        int _2_at = 0;
+        boolean _3 = false;
+        int _3_at = 0;
+
+        for (int i = 0; i < tallies.length; i++)
+            if (tallies[i] == 2) {
+                _2 = true;
+                _2_at = i + 1;
+            }
+
+        for (int i = 0; i < tallies.length; i++)
+            if (tallies[i] == 3) {
+                _3 = true;
+                _3_at = i + 1;
+            }
+
+        if (_2 && _3)
+            return _2_at * 2 + _3_at * 3;
+        else
+            return 0;
+    }
+}
--- src/main/java/Yatzy.java
+++ src/main/java/Yatzy.java
@@ -67,38 +67,8 @@ public class Yatzy {
     }
 
     public int fullHouse() {
-        var tallies = new ScoreUtils().buildIntArrayFromParametersList(dices);
-        boolean _2 = false;
-        int _2_at = 0;
-        boolean _3 = false;
-        int _3_at = 0;
-
-        for (int i = 0; i < tallies.length; i++)
-            if (tallies[i] == 2) {
-                _2 = true;
-                _2_at = i + 1;
-            }
-
-        for (int i = 0; i < tallies.length; i++)
-            if (tallies[i] == 3) {
-                _3 = true;
-                _3_at = i + 1;
-            }
-
-        if (_2 && _3)
-            return _2_at * 2 + _3_at * 3;
-        else
-            return 0;
-    }
-
-    private int computeSumOf(int countedValue) {
-        int sum = 0;
-        for (int die : dices) {
-            if (die == countedValue) {
-                sum += countedValue;
-            }
-        }
-        return sum;
+        var scoreCalculator = new FullHouseScoreCalculator(dices);
+        return scoreCalculator.computeScore();
     }
 
     public int fours() {
