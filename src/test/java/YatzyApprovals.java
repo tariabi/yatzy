c1e76b3d3d2c0e9574bb25c2c55823224fc7d60d
tarek <tarekriabi@gmail.com>
2024-03-23 07:36:27 +0100

    added approval for largeStraight

--- src/test/java/YatzyApprovals.java
+++ src/test/java/YatzyApprovals.java
@@ -218,4 +218,21 @@ public class YatzyApprovals {
             diceValues5
         );
     }
+
+    @Test
+    void approvals_verify_largeStraight() {
+        var diceValues1 = new Integer[]{1,2,3,4,5,6};
+        var diceValues2 = new Integer[]{1,2,3,4,5,6};
+        var diceValues3 = new Integer[]{1,2,3,4,5,6};
+        var diceValues4 = new Integer[]{1,2,3,4,5,6};
+        var diceValues5 = new Integer[]{1,2,3,4,5,6};
+        CombinationApprovals.verifyAllCombinations(
+            Yatzy::largeStraight,
+            diceValues1,
+            diceValues2,
+            diceValues3,
+            diceValues4,
+            diceValues5
+        );
+    }
 }
