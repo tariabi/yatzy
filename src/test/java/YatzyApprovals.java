0596cb7aa0cf0cf048e11a73c2925fdbd7b40c88
tarek <tarekriabi@gmail.com>
2024-03-23 07:37:46 +0100

    added approval for fullHouse

--- src/test/java/YatzyApprovals.java
+++ src/test/java/YatzyApprovals.java
@@ -235,4 +235,21 @@ public class YatzyApprovals {
             diceValues5
         );
     }
+
+    @Test
+    void approvals_verify_fullHouse() {
+        var diceValues1 = new Integer[]{1,2,3,4,5,6};
+        var diceValues2 = new Integer[]{1,2,3,4,5,6};
+        var diceValues3 = new Integer[]{1,2,3,4,5,6};
+        var diceValues4 = new Integer[]{1,2,3,4,5,6};
+        var diceValues5 = new Integer[]{1,2,3,4,5,6};
+        CombinationApprovals.verifyAllCombinations(
+            Yatzy::fullHouse,
+            diceValues1,
+            diceValues2,
+            diceValues3,
+            diceValues4,
+            diceValues5
+        );
+    }
 }
