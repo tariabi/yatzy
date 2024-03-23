1a20b22b329b08a7b070f047efbcbe3794830940
tarek <tarekriabi@gmail.com>
2024-03-23 07:12:54 +0100

    added approval for twos

--- src/test/java/YatzyApprovals.java
+++ src/test/java/YatzyApprovals.java
@@ -50,4 +50,20 @@ public class YatzyApprovals {
             diceValues5
         );
     }
+
+    @Test
+    void approvals_verify_twos() {
+        var diceValues1 = new Integer[]{1,2,3,4,5,6};
+        var diceValues2 = new Integer[]{1,2,3,4,5,6};
+        var diceValues3 = new Integer[]{1,2,3,4,5,6};
+        var diceValues4 = new Integer[]{1,2,3,4,5,6};
+        var diceValues5 = new Integer[]{1,2,3,4,5,6};
+        CombinationApprovals.verifyAllCombinations(Yatzy::twos,
+            diceValues1,
+            diceValues2,
+            diceValues3,
+            diceValues4,
+            diceValues5
+        );
+    }
 }
