14e80268dd0604816991a3317f8adcebda6d7b25
tarek <tarekriabi@gmail.com>
2024-03-23 08:32:55 +0100

    fixed another inconsistent parameter name

--- src/main/java/Yatzy.java
+++ src/main/java/Yatzy.java
@@ -133,12 +133,12 @@ public class Yatzy {
             return 0;
     }
 
-    public static int four_of_a_kind(int _1, int _2, int d3, int d4, int d5)
+    public static int four_of_a_kind(int d1, int d2, int d3, int d4, int d5)
     {
         int[] tallies;
         tallies = new int[6];
-        tallies[_1-1]++;
-        tallies[_2-1]++;
+        tallies[d1-1]++;
+        tallies[d2-1]++;
         tallies[d3-1]++;
         tallies[d4-1]++;
         tallies[d5-1]++;
