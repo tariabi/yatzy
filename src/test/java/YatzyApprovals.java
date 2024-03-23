b2267299ef1281669abd953e12f9a1210eaeff65
tarek <tarekriabi@gmail.com>
2024-03-23 07:26:23 +0100

    added approval for pairs

--- src/test/java/YatzyApprovals.java
+++ src/test/java/YatzyApprovals.java
@@ -133,4 +133,21 @@ public class YatzyApprovals {
             diceValues5
         );
     }
+
+    @Test
+    void approvals_verify_score_pair() {
+        var diceValues1 = new Integer[]{1,2,3,4,5,6};
+        var diceValues2 = new Integer[]{1,2,3,4,5,6};
+        var diceValues3 = new Integer[]{1,2,3,4,5,6};
+        var diceValues4 = new Integer[]{1,2,3,4,5,6};
+        var diceValues5 = new Integer[]{1,2,3,4,5,6};
+        CombinationApprovals.verifyAllCombinations(
+            Yatzy::score_pair,
+            diceValues1,
+            diceValues2,
+            diceValues3,
+            diceValues4,
+            diceValues5
+        );
+    }
 }
