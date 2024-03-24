5983f1e17caab30f2b1ace0cb5c708af9a141672
tarek <tarekriabi@gmail.com>
2024-03-24 09:21:29 +0100

    added dice rolls builder from roll values

--- src/main/java/Yatzy.java
+++ src/main/java/Yatzy.java
@@ -6,13 +6,7 @@ public class Yatzy {
     private final DiceRolls dice;
 
     public Yatzy(int dice1, int dice2, int dice3, int dice4, int dice5) {
-        dice = new DiceRolls(
-            new DieRoll[]{new DieRoll(dice1),
-            new DieRoll(dice2),
-            new DieRoll(dice3),
-            new DieRoll(dice4),
-            new DieRoll(dice5)}
-        );
+        dice = DiceRolls.fromRolls(dice1, dice2, dice3, dice4, dice5);
     }
 
     public int chance() {
--- src/main/java/model/DiceRolls.java
+++ src/main/java/model/DiceRolls.java
@@ -32,11 +32,19 @@ public class DiceRolls {
     public Map<DieRoll, Integer> buildRollsOccurrenceMap() {
         Map<DieRoll, Integer> map = new HashMap<>();
         for (DieRoll dieRoll: rolls) {
-            if (!map.containsKey(dieRoll)) {
+            if (!map.containsKey(dieRoll))
                 map.put(dieRoll, 0);
-            }
             map.put(dieRoll, 1 + map.get(dieRoll));
         }
         return map;
+    }
+
+    public static DiceRolls fromRolls(int... rolls) {
+        if (rolls.length != 5)
+            throw new IllegalArgumentException("You must provide exactly 5 rolls");
+        DieRoll[] tempRolls = new DieRoll[5];
+        for (int i = 0; i < rolls.length; i++)
+            tempRolls[i] = new DieRoll(rolls[i]);
+        return new DiceRolls(tempRolls);
     }
 }
